1e21143224eb81807fffed77223e9e77d486a74e
Indra Lukmana <indra.lukmana.il@gmail.com>
2018-07-02 21:29:54 +0700

    Add custom header

--- header.php
+++ header.php
@@ -24,6 +24,17 @@
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'chainx' ); ?></a>
 
+	<?php if (get_header_image()): ?>
+
+	<figure class="header-image">
+
+		<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home" >
+			<?php the_header_image_tag(); ?>
+		</a>
+
+	</figure>
+	<?php endif; ?>
+
 	<header id="masthead" class="site-header">
 		<div class="site-branding">
 			<?php
