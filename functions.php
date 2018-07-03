bf72d943e13c8b1d2301d1e4b68cfc7c326db551
Indra Lukmana <indra.lukmana.il@gmail.com>
2018-07-03 13:59:43 +0700

    Rename primary menu to header

--- functions.php
+++ functions.php
@@ -44,7 +44,7 @@ if ( ! function_exists( 'chainx_setup' ) ) :
 
 		// This theme uses wp_nav_menu() in one location.
 		register_nav_menus( array(
-			'menu-1' => esc_html__( 'Primary', 'chainx' ),
+			'menu-1' => esc_html__( 'Header', 'chainx' ),
 		) );
 
 		/*
