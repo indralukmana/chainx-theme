57b1d10ab0fd0565a27acf256719704efa4de347
Indra Lukmana <indra.lukmana.il@gmail.com>
2018-07-05 08:57:50 +0700

    Change category listings position

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -53,17 +53,11 @@ endif;
 
 if ( ! function_exists( 'chainx_entry_footer' ) ) :
 	/**
-	 * Prints HTML with meta information for the categories, tags and comments.
+	 * Prints HTML with meta information for the tags and comments.
 	 */
 	function chainx_entry_footer() {
 		// Hide category and tag text for pages.
 		if ( 'post' === get_post_type() ) {
-			/* translators: used between list items, there is a space after the comma */
-			$categories_list = get_the_category_list( esc_html__( ', ', 'chainx' ) );
-			if ( $categories_list ) {
-				/* translators: 1: list of categories. */
-				printf( '<span class="cat-links">' . esc_html__( 'Posted in %1$s', 'chainx' ) . '</span>', $categories_list ); // WPCS: XSS OK.
-			}
 
 			/* translators: used between list items, there is a space after the comma */
 			$tags_list = get_the_tag_list( '', esc_html_x( ', ', 'list item separator', 'chainx' ) );
@@ -110,6 +104,15 @@ if ( ! function_exists( 'chainx_entry_footer' ) ) :
 		);
 	}
 endif;
+
+function chainx_category_list(){
+	/* translators: used between list items, there is a space after the comma */
+	$categories_list = get_the_category_list( esc_html__( ', ', 'chainx' ) );
+	if ( $categories_list ) {
+		/* translators: 1: list of categories. */
+		printf( '<span class="cat-links">' . esc_html__( '%1$s', 'chainx' ) . '</span>', $categories_list ); // WPCS: XSS OK.
+	}
+}
 
 if ( ! function_exists( 'chainx_post_thumbnail' ) ) :
 	/**
