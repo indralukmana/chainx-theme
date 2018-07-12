35f8d95ac595adc1d9fbff8e870ef4bca90a6644
Indra Lukmana <indra.lukmana.il@gmail.com>
2018-07-12 15:22:01 +0700

    Remove top comments navigation

--- comments.php
+++ comments.php
@@ -46,8 +46,6 @@ if ( post_password_required() ) {
 			?>
 		</h2><!-- .comments-title -->
 
-		<?php the_comments_navigation(); ?>
-
 		<ol class="comment-list">
 			<?php
 			wp_list_comments( array(
