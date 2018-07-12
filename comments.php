c20beb5faafc864b88b72dcf6e6c59eb2056de05
Indra Lukmana <indra.lukmana.il@gmail.com>
2018-07-12 15:17:09 +0700

    Change comment title wording

--- comments.php
+++ comments.php
@@ -32,13 +32,13 @@ if ( post_password_required() ) {
 			if ( '1' === $chainx_comment_count ) {
 				printf(
 					/* translators: 1: title. */
-					esc_html__( 'One thought on &ldquo;%1$s&rdquo;', 'chainx' ),
+					esc_html__( 'One comment', 'chainx' ),
 					'<span>' . get_the_title() . '</span>'
 				);
 			} else {
 				printf( // WPCS: XSS OK.
 					/* translators: 1: comment count number, 2: title. */
-					esc_html( _nx( '%1$s thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', $chainx_comment_count, 'comments title', 'chainx' ) ),
+					esc_html( _nx( '%1$s comment', '%1$s comments', $chainx_comment_count, 'comments title', 'chainx' ) ),
 					number_format_i18n( $chainx_comment_count ),
 					'<span>' . get_the_title() . '</span>'
 				);
