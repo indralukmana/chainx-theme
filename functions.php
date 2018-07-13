3f91e0c3022c917e9ee90c2c8d7e77ef8df7ddf5
Indra Lukmana <indra.lukmana.il@gmail.com>
2018-07-13 21:08:49 +0700

    Add widget to the footer

--- functions.php
+++ functions.php
@@ -173,6 +173,18 @@ function chainx_widgets_init() {
 		'before_title'  => '<h2 class="widget-title">',
 		'after_title'   => '</h2>',
 	) );
+
+	register_sidebar( array(
+		'name'          => esc_html__( 'Footer Widget', 'chainx' ),
+		'id'            => 'footer-1',
+		'description'   => esc_html__( 'Add widgets here.', 'chainx' ),
+		'before_widget' => '<section id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</section>',
+		'before_title'  => '<h2 class="widget-title">',
+		'after_title'   => '</h2>',
+	) );
+
+	
 }
 add_action( 'widgets_init', 'chainx_widgets_init' );
 
