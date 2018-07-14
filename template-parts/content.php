f25b9bb6cd95529129c47d01580e06bb1857158c
Indra Lukmana <indra.lukmana.il@gmail.com>
2018-07-14 09:31:45 +0700

    Add continue reading link and style

--- template-parts/content.php
+++ template-parts/content.php
@@ -59,5 +59,26 @@
 			?>
 		</div><!-- .entry-content -->
 
+		<div class="continue-reading">
+			<?php
+			$more_link = sprintf(
+				wp_kses(
+					/* translators: %s: Name of current post. Only visible to screen readers */
+					__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'chainx' ),
+					array(
+						'span' => array(
+							'class' => array(),
+						),
+					)
+				),
+				get_the_title()
+			);
+			?>
+
+			<a href=' <?php echo esc_url( get_permalink() ) ?> ' rel="bookmark">
+				<?php echo $more_link ?>
+			</a>
+		</div> <!-- .continue-reading -->
+
 	</div>
 </article><!-- #post-<?php the_ID(); ?> -->
