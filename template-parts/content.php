3551956cade09ec65bc5be4f387a7d4978c19b9a
Indra Lukmana <indra.lukmana.il@gmail.com>
2018-07-14 09:35:22 +0700

    Change the content into excerpt

--- template-parts/content.php
+++ template-parts/content.php
@@ -39,23 +39,7 @@
 
 		<div class="entry-content">
 			<?php
-			the_content( sprintf(
-				wp_kses(
-					/* translators: %s: Name of current post. Only visible to screen readers */
-					__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'chainx' ),
-					array(
-						'span' => array(
-							'class' => array(),
-						),
-					)
-				),
-				get_the_title()
-			) );
-
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'chainx' ),
-				'after'  => '</div>',
-			) );
+				the_excerpt();
 			?>
 		</div><!-- .entry-content -->
 
