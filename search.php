aa3bb9431f0c86722617823c861dc72eafc6f226
Indra Lukmana <indra.lukmana.il@gmail.com>
2018-07-14 21:13:21 +0700

    Change the search template to use the regular content

--- search.php
+++ search.php
@@ -20,7 +20,7 @@ get_header();
 					?>
 				</h1>
 			</header><!-- .page-header -->
-			
+
 	<?php endif; ?>
 
 	<section id="primary" class="content-area">
@@ -38,7 +38,7 @@ get_header();
 				 * If you want to overload this in a child theme then include a file
 				 * called content-search.php and that will be used instead.
 				 */
-				get_template_part( 'template-parts/content', 'search' );
+				get_template_part( 'template-parts/content' );
 
 			endwhile;
 
