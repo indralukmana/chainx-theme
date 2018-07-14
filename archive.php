1c8e13bf4d80cd2bf00cf3d5e9dfda4fcb05f892
Indra Lukmana <indra.lukmana.il@gmail.com>
2018-07-14 20:48:25 +0700

    Style the header in archive

--- archive.php
+++ archive.php
@@ -10,17 +10,20 @@
 get_header();
 ?>
 
+	<?php if (have_posts() ): ?>
+		<header class="page-header">
+			<?php
+			the_archive_title( '<h1 class="page-title">', '</h1>' );
+			the_archive_description( '<div class="archive-description">', '</div>' );
+			?>
+		</header><!-- .page-header -->
+
+	<?php endif; ?>
+
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
 
 		<?php if ( have_posts() ) : ?>
-
-			<header class="page-header">
-				<?php
-				the_archive_title( '<h1 class="page-title">', '</h1>' );
-				the_archive_description( '<div class="archive-description">', '</div>' );
-				?>
-			</header><!-- .page-header -->
 
 			<?php
 			/* Start the Loop */
