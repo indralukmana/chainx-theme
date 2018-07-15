9023f7c63966aa87bc2bd1f84ab330d27ba7f95a
Indra Lukmana <indra.lukmana.il@gmail.com>
2018-07-15 11:33:24 +0700

    Register new widget for page

--- functions.php
+++ functions.php
@@ -185,6 +185,15 @@ function chainx_widgets_init() {
 		'after_title'   => '</h2>',
 	) );
 
+	register_sidebar( array(
+		'name'          => esc_html__( 'Page Widget', 'chainx' ),
+		'id'            => 'page-1',
+		'description'   => esc_html__( 'Add widgets here.', 'chainx' ),
+		'before_widget' => '<section id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</section>',
+		'before_title'  => '<h2 class="widget-title">',
+		'after_title'   => '</h2>',
+	) );
 	
 }
 add_action( 'widgets_init', 'chainx_widgets_init' );
