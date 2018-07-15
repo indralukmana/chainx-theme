ec96f3e04d5a0ed65962ab947b86bb116d3b0316
Indra Lukmana <indra.lukmana.il@gmail.com>
2018-07-15 15:06:02 +0700

    Add functions from Twenty Seventeen for SVG icons

--- functions.php
+++ functions.php
@@ -321,6 +321,12 @@ require get_template_directory() . '/inc/template-functions.php';
 require get_template_directory() . '/inc/customizer.php';
 
 /**
+ * SVG functions
+ */
+require get_template_directory() . '/inc/icon-functions.php';
+
+
+/**
  * Load Jetpack compatibility file.
  */
 if ( defined( 'JETPACK__VERSION' ) ) {
