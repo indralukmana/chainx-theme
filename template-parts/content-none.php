b78bda5bd7badd163a0fd3a23ab0d5d1c0664432
Indra Lukmana <indra.lukmana.il@gmail.com>
2018-07-15 08:59:48 +0700

    Layout the content-none

--- template-parts/content-none.php
+++ template-parts/content-none.php
@@ -27,40 +27,78 @@
 	</h1>
 </header><!-- .page-header -->
 
-<section class="no-results not-found">
+<section id="primary" class="content-area not-found <?php if( is_404() ){ echo 'error-404'; } else { echo 'no-results'; } ?>">
+	<main id="main" class="site-main" role="main">
+		<div class="page-content">
+			<?php
+			if ( is_home() && current_user_can( 'publish_posts' ) ) :
 
-	<div class="page-content">
+				printf(
+					'<p>' . wp_kses(
+						/* translators: 1: link to WP admin new post page. */
+						__( 'Ready to publish your first post? <a href="%1$s">Get started here</a>.', 'chainx' ),
+						array(
+							'a' => array(
+								'href' => array(),
+							),
+						)
+					) . '</p>',
+					esc_url( admin_url( 'post-new.php' ) )
+				);
+
+			elseif ( is_search() ) :
+				?>
+
+				<p><?php esc_html_e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'chainx' ); ?></p>
+				<?php
+				get_search_form();
+
+			
+			elseif ( is_404() ) :
+					?>
+	
+					<p><?php esc_html_e( 'Sorry, the page you are looking for does not exist. Check out the recent articles or do a search below.', 'chainx' ); ?></p>
+					<?php
+					get_search_form();
+				
+			else :
+				?>
+
+				<p><?php esc_html_e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'chainx' ); ?></p>
+				<?php
+				get_search_form();
+
+			endif;
+			?>
+		</div><!-- .page-content -->
+
 		<?php
-		if ( is_home() && current_user_can( 'publish_posts' ) ) :
-
-			printf(
-				'<p>' . wp_kses(
-					/* translators: 1: link to WP admin new post page. */
-					__( 'Ready to publish your first post? <a href="%1$s">Get started here</a>.', 'chainx' ),
-					array(
-						'a' => array(
-							'href' => array(),
-						),
-					)
-				) . '</p>',
-				esc_url( admin_url( 'post-new.php' ) )
-			);
-
-		elseif ( is_search() ) :
-			?>
-
-			<p><?php esc_html_e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'chainx' ); ?></p>
-			<?php
-			get_search_form();
-
-		else :
-			?>
-
-			<p><?php esc_html_e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'chainx' ); ?></p>
-			<?php
-			get_search_form();
-
-		endif;
+			if ( is_404() || is_search() ) {
 		?>
-	</div><!-- .page-content -->
+				<h2 class="page-title secondary-title"><?php esc_html_e( 'Recent posts:', 'chainx'); ?></h2>
+
+		<?php
+				$query = array(
+					'posts_per_page' => 6
+				);
+
+				$posts = new WP_Query( $query );
+
+				if( $posts->have_posts() ){
+					while ( $posts->have_posts() ){
+						$posts->the_post();
+						get_template_part( 'template-parts/content', get_post_format() );
+					}
+				}
+
+				wp_reset_postdata();
+			}
+		?>
+	</main> <!-- #main -->
+
 </section><!-- .no-results -->
+
+<?php
+
+get_sidebar();
+get_footer();
