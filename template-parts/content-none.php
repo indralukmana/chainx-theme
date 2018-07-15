5108df8bf663f575fe1bd340ffd60471a6f798d1
Indra Lukmana <indra.lukmana.il@gmail.com>
2018-07-15 08:28:25 +0700

    Configure header for content-none

--- template-parts/content-none.php
+++ template-parts/content-none.php
@@ -9,10 +9,25 @@
 
 ?>
 
+<header class="page-header">
+	<h1 class="page-title">
+		<?php 
+			if ( is_404() ) {
+				esc_html_e( 'Page is not available', 'chainx' );
+			} else if( is_search() ) {
+				/* translators: %s = search query */
+				printf( 
+					esc_html__( 'We cannot find &ldquo;%s&rdquo;', 'chainx' ),
+					get_search_query()	
+				);
+			} else {
+				esc_html_e( 'Nothing Found', 'chainx' );
+			}
+		?>
+	</h1>
+</header><!-- .page-header -->
+
 <section class="no-results not-found">
-	<header class="page-header">
-		<h1 class="page-title"><?php esc_html_e( 'Nothing Found', 'chainx' ); ?></h1>
-	</header><!-- .page-header -->
 
 	<div class="page-content">
 		<?php
